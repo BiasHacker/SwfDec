20185ae15b25ebcafb913a0868061891fbb74596
Sin <null>
2018-12-02 22:55:54 +0900

    不足分のクラスファイルを追加

---
+++ AVM2/ByteCode/Instructions/Debug/As3DebugFile.cs
@@ -0,0 +1,19 @@
+﻿namespace SwfDec.AVM2.ByteCode.Instructions.Debug
+{
+    // 用途が不明なためハリボテ実装
+
+    class As3DebugFile : As3Instruction
+    {
+        public override Opcode Opcode => Opcode.DebugLine;
+
+        public As3DebugFile(uint obj)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
---
+++ AVM2/ByteCode/Instructions/Debug/As3DebugLine.cs
@@ -0,0 +1,19 @@
+﻿namespace SwfDec.AVM2.ByteCode.Instructions.Debug
+{
+    // 用途が不明なためハリボテ実装
+
+    class As3DebugLine : As3Instruction
+    {
+        public override Opcode Opcode => Opcode.DebugLine;
+
+        public As3DebugLine(uint obj)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
